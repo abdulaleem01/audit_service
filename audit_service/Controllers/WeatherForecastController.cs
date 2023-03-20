d464a750973b7ac8bc2cd30686cbbbb67d1a0697
abdulaleem01 <102243797+abdulaleem01@users.noreply.github.com>
2023-03-20 23:39:50 +0530

    new commit

--- audit_service/Controllers/WeatherForecastController.cs
+++ audit_service/Controllers/WeatherForecastController.cs
@@ -29,5 +29,17 @@ public class WeatherForecastController : ControllerBase
         })
         .ToArray();
     }
+
+       [HttpGet(Name = "GetWeatherFore")]
+    public IEnumerable<WeatherForecast> Gett()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
 }
 
